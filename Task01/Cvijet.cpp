6e7b885de0331f588a8396b7f34120b56a7aa9f0
Antonio Plečko Marović <aplecko@ampeu.hr>
2025-04-11 22:30:25 +0200

    Ispravljen cvijet

--- Task01/Cvijet.cpp
+++ Task01/Cvijet.cpp
@@ -2,9 +2,35 @@
 
 Cvijet::Cvijet(sf::RenderWindow& window) : window(window)
 {
-    sunce.setRadius(20.f);
+    // Malo sunce u kutu
+    sunce.setRadius(10.f);
     sunce.setFillColor(sf::Color::Yellow);
-    sunce.setPosition({ 190.f, 100.f }); // novi stil inicijalizacije
+    sunce.setPosition({ 10.f, 10.f });
+
+    // Žuta sredina cvijeta
+    sredinaCvijeta.setRadius(50.f);
+    sredinaCvijeta.setFillColor(sf::Color::Yellow);
+    sredinaCvijeta.setPosition({ 150.f, 100.f }); // centrirano u prozoru
+
+    // Crvene latice (prsten oko žute sredine)
+    latice.setRadius(70.f);
+    latice.setFillColor(sf::Color::Red);
+    latice.setPosition({ 130.f, 80.f }); // oko sredine
+    latice.setPointCount(100); // da krug bude lijepo gladak
+
+    // Stabljika
+    stabljika.setSize({ 10.f, 100.f });
+    stabljika.setFillColor(sf::Color::Green);
+    stabljika.setPosition({ 185.f, 200.f });
+
+    // List (koso)
+    list.setPointCount(4);
+    list.setPoint(0, { 0.f, 0.f });
+    list.setPoint(1, { 50.f, -20.f });
+    list.setPoint(2, { 80.f, 0.f });
+    list.setPoint(3, { 50.f, 20.f });
+    list.setFillColor(sf::Color::Green);
+    list.setPosition({ 190.f, 250.f });
 
     smanjuje = true;
 }
@@ -14,17 +40,26 @@ void Cvijet::draw()
     if (clock.getElapsedTime().asMilliseconds() > 100)
     {
         if (smanjuje)
-            sunce.setRadius(sunce.getRadius() - 0.5f);
+            sunce.setRadius(sunce.getRadius() - 0.2f);
         else
-            sunce.setRadius(sunce.getRadius() + 0.5f);
+            sunce.setRadius(sunce.getRadius() + 0.2f);
 
-        if (sunce.getRadius() <= 15.f)
+        if (sunce.getRadius() <= 8.f)
             smanjuje = false;
-        if (sunce.getRadius() >= 20.f)
+        if (sunce.getRadius() >= 10.f)
             smanjuje = true;
 
         clock.restart();
     }
 
+    // Prvo crtamo latice (iza)
+    window.draw(latice);
+    // Onda sredinu cvijeta
+    window.draw(sredinaCvijeta);
+    // Onda stabljiku
+    window.draw(stabljika);
+    // Onda list
+    window.draw(list);
+    // I na kraju malo sunce
     window.draw(sunce);
 }
--- Task01/cvijet.h
+++ Task01/cvijet.h
@@ -5,11 +5,15 @@ class Cvijet
 {
 private:
     sf::RenderWindow& window;
-    sf::CircleShape sunce;
+    sf::CircleShape sunce; // malo sunce gore lijevo
+    sf::CircleShape sredinaCvijeta; // žuti centar cvijeta
+    sf::CircleShape latice; // crveni "prsten" oko centra
+    sf::RectangleShape stabljika; // stabljika
+    sf::ConvexShape list; // list
     sf::Clock clock;
     bool smanjuje;
 
 public:
-    Cvijet(sf::RenderWindow& window); // referenca, ne pointer
+    Cvijet(sf::RenderWindow& window);
     void draw();
 };
